ccca2fc69f2233bcffb38dc1c4e2372bab5f9247
unknown <nitinsharmauc@gmail.com>
2021-01-03 21:44:20 +0530

    Add the request validator

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -7,12 +7,14 @@ import { createTodo } from '../../businessLogic/todos'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  console.log("Creating todo", newTodo)
   const authorization = event.headers.Authorization
   const split = authorization.split(' ')
   const jwtToken = split[1]
 
   // TODO: Implement creating a new TODO item
   const newtodo = await createTodo(newTodo, jwtToken)
+  console.log("Created todo", newTodo)
   //console.log(todos)
   return {
     statusCode: 201,
